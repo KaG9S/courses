7b3b128a5bd8090c4e629062c4256212592d3470
George Kanivetc <150721213+KaG9S@users.noreply.github.com>
2024-12-24 21:49:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function courses() {
+    inner = '<a href="courses/HTML/index.html">HTML</a>'
+    inner += '<a href="courses/CSS/index.html">CSS</a>'
+    inner += '<a href="courses/JavaScript/index.html">JavaScript</a>'
+    inner += '<a href="courses/Web/index.html">Web</a>'
+    document.getElementById('courses').innerHTML = inner
+}
