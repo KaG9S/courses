aaee5606eec7b7dc68d8a4bcf7f46a5ac2f18cc2
George Kanivetc <150721213+KaG9S@users.noreply.github.com>
2024-12-25 18:59:09 +0200

    .

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+class headers{
+    constructor(id, inner = null, innerWhenOn = null, on = false){
+        this.id = id
+        this.inner = inner
+        this.innerWhenOn = innerWhenOn
+        this.on = on
+    }
+    show(){
+        document.getElementById(this.id).innerHTML = this.innerWhenOn
+    }
+    hide(){
+        document.getElementById(this.id).innerHTML = null
+    }
+}
+
 function courses() {
     inner = '<a href="courses/HTML/index.html">HTML</a>'
     inner += '<a href="courses/CSS/index.html">CSS</a>'
